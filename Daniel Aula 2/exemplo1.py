f736d674dee5add0ea1fcfd80746546379a1faf8
DanSouKK <dansouzaofficial@gmail.com>
2024-10-02 19:29:48 -0300

    Update exemplo1.py

--- Daniel Aula 2/exemplo1.py
+++ Daniel Aula 2/exemplo1.py
@@ -1,5 +1,8 @@
-# estes serao os passos do pirata Kunkka
+# estes serao os passos do pirata Kunkka em busco do tesouro perdido
 print("Andar para esquerda")
 print("Andar para baixo")
 print("Andar para cima")
 print("Andar para direita")
+#apos achar o tesouro "X" e cavar
+print("procurar o X")
+print("cavar")
