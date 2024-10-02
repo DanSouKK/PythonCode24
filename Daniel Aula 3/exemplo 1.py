e7d54152596e28e0ff621dd9e6229c8e705bcd15
DanSouKK <dansouzaofficial@gmail.com>
2024-10-02 19:50:42 -0300

    exemplo1.py
    Calculando valores recebidos em cima de outras variaveis, fazendo calculos.

---
+++ Daniel Aula 3/exemplo 1.py
@@ -0,0 +1,5 @@
+#Calcular valores com base em outras variaveis
+print("Vamos calcular o imposto! Coloque o valor do produto")
+valor_produto = float(input("Coloque o valor do produto: $"))
+imposto = valor_produto * 0.1
+print(f"O valor que voce devera pagar em impostos do produto é: ${imposto}")
