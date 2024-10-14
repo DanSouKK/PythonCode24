9b829741fe5eb34dbaa673c5d51cfe91995eb5a1
Daniel Souza <dansouzaofficial@gmail.com>
2024-10-14 16:55:29 -0300

    Update Contador de numeros pares e impares.py

--- Daniel Projetos em Casa/Contador de numeros pares e impares.py
+++ Daniel Projetos em Casa/Contador de numeros pares e impares.py
@@ -7,7 +7,7 @@ soma_impares = 0
 print("Digite um número (ou um número negativo para encerrar): ")
 
 while True:
-    numero = int(input())
+    numero = int(input("Digite aqui: "))
     
     if numero < 0:
         break  # Encerra o loop se o número for negativo    
@@ -15,14 +15,14 @@ while True:
     # Verifica se o número é par ou ímpar
     if numero % 2 == 0:
         contagem_pares += 1 
+        #fazer calculo dos valores inseridos
         soma_pares += numero   
         #verifica se a contagem de pares é multiplo de 5
         if contagem_pares % 5 == 0:
             print("Vi que você gosta dos numeros pares!")
     else:
         contagem_impares += 1
-        soma_impares += numero
-        #verifica se a contagem de pares é multipla de 5
+        soma_impares += numero        
         if contagem_impares % 5 == 0:
             print("Vi que você gosta dos numeros impares!")
 
@@ -30,4 +30,4 @@ while True:
 print(f"Números pares: {contagem_pares}")
 print(f"Soma dos seus numeros pares digitado: {soma_pares}")
 print(f"Números ímpares: {contagem_impares}")
-print(f"Soma dos seus numeros pares digitado: {soma_impares}")
+print(f"Soma dos seus numeros impares digitado: {soma_impares}")
