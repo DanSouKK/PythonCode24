ffea4f618db64043af81c1d906461ff4743a3337
Daniel Souza <dansouzaofficial@gmail.com>
2024-10-23 14:41:21 -0300

    Create minhas_funcoes.py
    adiçao de novas funções para uma calculadora basica.

---
+++ Daniel Aula 11/exemplos_2/minhas_funcoes.py
@@ -0,0 +1,17 @@
+# minhas_funcoes.py
+
+def cumprimentar():
+    print("Olá! Seja bem-vindo ao programa.")
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
