ecc1cebbd8944317df777e3e0fcf7d455f55c001
DanSouKK <dansouzaofficial@gmail.com>
2024-10-25 21:35:22 -0300

    Update criação_personagem.py

--- Daniel Projetos em Casa/RPG_do_aventureiro/criação_personagem.py
+++ Daniel Projetos em Casa/RPG_do_aventureiro/criação_personagem.py
@@ -21,7 +21,7 @@ def gerar_nome():
 #definindo raça entre humanos, elfos, orc, anao, halflings
 def criar_personagem_aleatorio():
     racas = ["humano", "elfo", "orc", "anao", "halfling"]
-    raca = input("Escolha sua raça:\n'Humano' - (sem bonus)\n'Elfo' - (-1 CON /-1STR /+3 DEX)\n'Orc' - (+3 CON/+1 STR /-2 DEX)\n'Anao' - (+1 CON /+1 STR /-2 DEX)\n'Halfling' - (-1 CON /-1 STR / +3 DEX)\n").lower()
+    raca = input("\nEscolha sua raça:\n'Humano' - (sem bonus)\n'Elfo' - (-1 CON /-1STR /+3 DEX)\n'Orc' - (+3 CON/+1 STR /-2 DEX)\n'Anao' - (+1 CON /+1 STR /-2 DEX)\n'Halfling' - (-1 CON /-1 STR / +3 DEX)\n").lower()
 
     if raca not in racas:
         print(f"Raça inválida '{raca}'. Uma raça aleatória será escolhida.")
@@ -102,3 +102,43 @@ def random_enemy(x=1, y=20): # Valores padrão definidos
 
     return enemy
 
+#exemplos de chamadas para o programa do rpg do aventureiro
+
+# Loop para obter a dificuldade do inimigo
+# caso necessario precisar no programa para eventuais tipos de inimigos.
+while True:
+    try:
+        opcao = input("\nDeseja definir a dificuldade do inimigo? (s/n): ").lower()
+        
+        if opcao == 's':
+            print("Nivel de dificuldade: 1 - Extremamente fácil / 20 - Mortalmente dificil.")
+            x = int(input("Digite o valor mínimo para o nível de dificuldade (ND) (1-20): "))
+            y = int(input("Digite o valor máximo para o nível de dificuldade (ND) (1-20): "))
+
+            # Verifica se os valores são válidos
+            if x < 1 or x > 20 or y < 1 or y > 20 or x > y:
+                print("Os valores devem estar entre 1 e 20 e o mínimo deve ser menor ou igual ao máximo.")
+                continue  # Retorna ao início do loop
+
+            break  # Sai do loop se os valores forem válidos
+        
+        elif opcao == 'n':
+            break  # Sai do loop se o usuário não deseja definir a dificuldade
+
+        else:
+            print("Opção inválida, por favor escolha 's' ou 'n'.")
+    
+    except ValueError:
+        print("Por favor, insira números válidos.")
+
+# Gera o inimigo com os valores válidos ou de forma padrao
+enemy_gerado = random_enemy(x, y)  # nome da função para gerar inimigo
+print("\nCaracterísticas do Inimigo:")
+for chave, valor in enemy_gerado.items():
+    print(f"{chave}: {valor}")
+
+#chamando a função e exibindo resultado do aventureiro
+personagem_gerado = criar_personagem_aleatorio()#nome da função para gerar aventureiro
+print("\nCaracterísticas do Personagem:")
+for chave, valor in personagem_gerado.items():
+    print(f"{chave}: {valor}")
